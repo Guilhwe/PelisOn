8db7141ad38268bf825dfe25fcdfd5214dba9cc7
Guilherme Melo <guilherme96melo@gmail.com>
2025-02-12 19:18:09 +0100

    Proyecto base con el que trabajaremos en clase Los ejercicios de las clases aparte del proyecto principal se haran en repositorios distintos.

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main() {
+        System.out.println("BIENVENIDOS A PelisON");
+        System.out.println("Película: Un monstruo viene a verme");
+        int año = 2016;
+        System.out.println("El año de estreno de la pelicula es "+ año);
+        boolean dentro_del_plan = true;
+        double nota = 9.9;
+        System.out.println(nota);
+        String resumen ="Monstruo Conoce Niño" ;
+        System.out.println(resumen);
+        
+
+
+    }
+    
+}
